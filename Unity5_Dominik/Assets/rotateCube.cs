26b0b85e8fae85880546925350401f6f5b15d63c
Kaint <winter@schule-raumberg.at>
2015-03-27 02:15:04 +0100

    eye tracking test

---
+++ Unity5_Dominik/Assets/rotateCube.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateCube : MonoBehaviour {
+	private GazeAwareComponent _gazeAware;
+
+	// Use this for initialization
+	void Start () {
+		_gazeAware = GetComponent<GazeAwareComponent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_gazeAware.HasGaze) {
+			transform.Rotate (Vector3.forward);
+		}
+	}
+}
