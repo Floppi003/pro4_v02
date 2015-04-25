441664dff2294c677b6d9d2455eb4f44b8eba0ec
Floppi003 <f.peinsold@gmail.com>
2015-04-26 01:09:39 +0200

    Added LeftEye RightEye Control
    Simple Version of left and right eye control implemented

--- Base2/Assets/Scripts/EyePositionDataComponent.cs
+++ Base2/Assets/Scripts/EyePositionDataComponent.cs
@@ -15,6 +15,9 @@ public class EyePositionDataComponent : MonoBehaviour
 
     private EyeXHost _eyexHost;
     private IEyeXDataProvider<EyeXEyePosition> _dataProvider;
+	private int eyesClosedCounter;
+	private int leftEyeOpenedCounter;
+	private int rightEyeOpenedCounter;
 
     /// <summary>
     /// Gets the last eye position.
@@ -23,6 +26,7 @@ public class EyePositionDataComponent : MonoBehaviour
 
     protected void Awake()
     {
+		eyesClosedCounter = 0;
         _eyexHost = EyeXHost.GetInstance();
         _dataProvider = _eyexHost.GetEyePositionDataProvider();
     }
@@ -63,14 +67,65 @@ public class EyePositionDataComponent : MonoBehaviour
 			//Debug.Log ("rightEyePosition: " + rightEyePosition);
 
 
+			if (leftEyePosition.Equals (new Vector3(0, 0, 0)) && rightEyePosition.Equals (new Vector3(0, 0, 0))) {
+				// Eyes closed
+				eyesClosedCounter++;
+				//Debug.Log ("!!! - !!! both eyes closed!!!!!!!!");
+
+			} else if (leftEyePosition.Equals (new Vector3(0, 0, 0)) && !rightEyePosition.Equals (new Vector3(0, 0, 0))) {
+				// only left eye is opened
+				this.leftEyeOpenedCounter++;
+				this.rightEyeOpenedCounter = this.rightEyeOpenedCounter / 3;
+				this.hideRightEyeObjects();
+				this.showLeftEyeObjects();
+
+
+			} else if (!leftEyePosition.Equals (new Vector3(0, 0, 0)) && rightEyePosition.Equals (new Vector3(0, 0, 0))) {
+				// only right eye is opened
+				this.rightEyeOpenedCounter++;
+				this.leftEyeOpenedCounter = this.leftEyeOpenedCounter / 3;
+				this.hideLeftEyeObjects ();
+				this.showRightEyeObjects ();
+
+			} else {
+				// both eyes opened
+				eyesClosedCounter = eyesClosedCounter / 3;
+
+				this.leftEyeOpenedCounter = this.leftEyeOpenedCounter / 2;
+				this.rightEyeOpenedCounter = this.rightEyeOpenedCounter / 2;
+				this.hideLeftEyeObjects();
+				this.hideRightEyeObjects ();
+			}
+
+			/*
+			if ((eyesClosedCounter > leftEyeOpenedCounter && eyesClosedCounter > rightEyeOpenedCounter) || 
+			    (eyesClosedCounter < 10 && leftEyeOpenedCounter < 10 && rightEyeOpenedCounter < 10)) {
+
+				// don't show anything
+				this.hideLeftEyeObjects();
+				this.hideRightEyeObjects();
+
+			} else if (leftEyeOpenedCounter > rightEyeOpenedCounter) {
+				// show only left objects
+				this.showLeftEyeObjects();
+				this.hideRightEyeObjects();
+
+			} else {
+				// show only right objects
+				this.showRightEyeObjects();
+				this.hideLeftEyeObjects();
+			}
+*/
+
+
+
+			// check whether bridge should be broad or not
 			// make bridge wider if both eyes are closed
 			GameObject bridge = GameObject.Find ("bridge");
 
-			if (leftEyePosition.Equals (new Vector3(0, 0, 0)) && rightEyePosition.Equals (new Vector3(0, 0, 0))) {
-				//Debug.Log ("!!! - !!! both eyes closed!!!!!!!!");
+			if (eyesClosedCounter > 24) {
 				bridge.transform.localScale = new Vector3(1, 1, 6);
 				GetComponent<BridgeCollider>().enabled = false;
-
 			} else {
 				bridge.transform.localScale = new Vector3(1, 1, 0.1f);
 				GetComponent<BridgeCollider>().enabled = true;
@@ -80,4 +135,42 @@ public class EyePositionDataComponent : MonoBehaviour
 			//Debug.Log ("leftEyePosition INVALID!");
 		}
     }
+
+
+	private void showLeftEyeObjects() {
+		GameObject[] leftEyeObjects = GameObject.FindGameObjectsWithTag ("LeftEye");
+		
+		foreach (GameObject leftEyeObject in leftEyeObjects) {
+			leftEyeObject.GetComponent<MeshCollider>().enabled = true;
+			leftEyeObject.GetComponent<MeshRenderer>().enabled = true;
+		}
+	}
+
+	private void showRightEyeObjects() {
+		GameObject[] rightEyeObjects = GameObject.FindGameObjectsWithTag ("RightEye");
+		
+		foreach (GameObject rightEyeObject in rightEyeObjects) {
+			rightEyeObject.GetComponent<MeshCollider>().enabled = true;
+			rightEyeObject.GetComponent<MeshRenderer>().enabled = true;
+		}
+	}
+
+	private void hideLeftEyeObjects() {
+		GameObject[] leftEyeObjects = GameObject.FindGameObjectsWithTag ("LeftEye");
+		
+		foreach (GameObject leftEyeObject in leftEyeObjects) {
+			leftEyeObject.GetComponent<MeshCollider>().enabled = false;
+			leftEyeObject.GetComponent<MeshRenderer>().enabled = false;
+		}
+	}
+
+	private void hideRightEyeObjects() {
+		GameObject[] rightEyeObjects = GameObject.FindGameObjectsWithTag ("RightEye");
+		
+		foreach (GameObject rightEyeObject in rightEyeObjects) {
+			rightEyeObject.GetComponent<MeshCollider>().enabled = false;
+			rightEyeObject.GetComponent<MeshRenderer>().enabled = false;
+		}
+	}
+
 }
