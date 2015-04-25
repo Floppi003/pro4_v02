0708733c358f93609c6c119be8656ecf707f04bc
Floppi003 <f.peinsold@gmail.com>
2015-04-25 22:20:18 +0200

    Added Bridge-Feature
    When closing the eyes the bridge will get wider.

---
+++ Base2/Assets/Scripts/BridgeCollider.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BridgeCollider : MonoBehaviour {
+
+	public string objectName;
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("OnTriggerEnter");
+		//other.gameObject.GetComponent<AudioSource> ().PlayOneShot (other.gameObject.GetComponent<FirstPersonController>().helloClip);
+	}
+	
+	void OnTriggerStay(Collider other) {
+		Debug.Log ("onTriggerStay");
+
+		// if it is the bridge, move the player slightly
+		if (objectName.Equals ("Bridge")) {
+			float randomNumber = Random.Range (-0.1f, 0.00f);
+			Vector3 position = other.gameObject.transform.position;
+			other.gameObject.transform.position = new Vector3 (position.x, position.y, position.z + randomNumber);
+
+		// if it is the floor below the bridge, move the player back to the beginning
+		} else if (objectName.Equals ("BridgeFloorless")) {
+			other.gameObject.transform.position = new Vector3(25.1f, 1.32f, -0.61f);
+		}
+
+
+	}
+	
+	void OnTriggerExit(Collider other) {
+		
+	}
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Base2/Assets/Scripts/EyePositionDataComponent.cs
@@ -0,0 +1,83 @@
+﻿//-----------------------------------------------------------------------
+// Copyright 2014 Tobii Technology AB. All rights reserved.
+//-----------------------------------------------------------------------
+
+using Tobii.EyeX.Framework;
+using UnityEngine;
+
+/// <summary>
+/// Component that encapsulates a provider for <see cref="EyeXEyePosition"/> data.
+/// </summary>
+[AddComponentMenu("Tobii EyeX/Eye Position Data")]
+public class EyePositionDataComponent : MonoBehaviour
+{
+    public FixationDataMode fixationDataMode;
+
+    private EyeXHost _eyexHost;
+    private IEyeXDataProvider<EyeXEyePosition> _dataProvider;
+
+    /// <summary>
+    /// Gets the last eye position.
+    /// </summary>
+    public EyeXEyePosition LastEyePosition { get; private set; }
+
+    protected void Awake()
+    {
+        _eyexHost = EyeXHost.GetInstance();
+        _dataProvider = _eyexHost.GetEyePositionDataProvider();
+    }
+
+    protected void OnEnable()
+    {
+        _dataProvider.Start();
+    }
+
+    protected void OnDisable()
+    {
+        _dataProvider.Stop();
+    }
+
+    protected void Update()
+    {
+        LastEyePosition = _dataProvider.Last;
+
+		// Get the last eye position.
+		EyeXEyePosition lastEyePosition = GetComponent<EyePositionDataComponent>().LastEyePosition;
+
+		if (lastEyePosition.IsValid) {
+			// Get the position of the left eye.
+			Vector3 leftEyePosition = new Vector3 (lastEyePosition.LeftEye.X,
+			                                       lastEyePosition.LeftEye.Y,
+			                                       lastEyePosition.LeftEye.Z);
+			
+			
+			// The Eye Position of a closed eye will be (0, 0, 0)
+			// Sometimes single eye positions can be (0, 0, 0) even though the eye was opened
+			
+			Vector3 rightEyePosition = new Vector3(lastEyePosition.RightEye.X,
+			                                       lastEyePosition.RightEye.Y,
+			                                       lastEyePosition.RightEye.Z);
+			
+			
+			//Debug.Log ("leftEyePosition: " + leftEyePosition);
+			//Debug.Log ("rightEyePosition: " + rightEyePosition);
+
+
+			// make bridge wider if both eyes are closed
+			GameObject bridge = GameObject.Find ("bridge");
+
+			if (leftEyePosition.Equals (new Vector3(0, 0, 0)) && rightEyePosition.Equals (new Vector3(0, 0, 0))) {
+				//Debug.Log ("!!! - !!! both eyes closed!!!!!!!!");
+				bridge.transform.localScale = new Vector3(1, 1, 6);
+				GetComponent<BridgeCollider>().enabled = false;
+
+			} else {
+				bridge.transform.localScale = new Vector3(1, 1, 0.1f);
+				GetComponent<BridgeCollider>().enabled = true;
+			}
+			
+		} else {
+			//Debug.Log ("leftEyePosition INVALID!");
+		}
+    }
+}
