9e41089994a0a55b20ffaf2f8a845faeba69edec
Carakai <lilli.luna@hotmail.de>
2015-04-28 16:39:03 +0200

    CameraVector auslesen

--- Base2/Assets/Scripts/Maze_Collider.cs
+++ Base2/Assets/Scripts/Maze_Collider.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class Maze_Collider : MonoBehaviour {
 
 	public Vector3 angle;
-	public Player player;
+	public Vector3 playerViewAngle;
+	public Camera cam;
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +21,8 @@ public class Maze_Collider : MonoBehaviour {
 		if (other.tag == "Player") {
 			Debug.Log ("Player detected");
 
+			playerViewAngle = cam.transform.rotation;
+			// quaternion to vector 3 oder so, zum vergleichen
 		}
 		Debug.Log ("OnTriggerStay");
 	}
